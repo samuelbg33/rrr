8d778a827643f1d8a2428f073108dbce2d8543bb
samuelbg33 <152225113+samuelbg33@users.noreply.github.com>
2024-05-02 22:18:39 -0500

    nuevo

--- src/Pages/Home/index.jsx
+++ src/Pages/Home/index.jsx
@@ -7,20 +7,21 @@ function Home() {
 
   useEffect(()=>{
     fetch('https://api.escuelajs.co/api/v1/products')
-    .then(response => response.json())
-    .then(data => setItems(data))
-
+     .then(response => response.json())
+     .then(data => setItems(data)) 
   }, [])
 
   return (
     <Layout>
       Home
+      <div className='grid gap-4 grid-cols-4 w-full max-w-sreen-lg'>
       {
-        items?.map(item =>(
+        items?.map(item => (
           <Card key={item.id} data={item} />
         ))
+          
       }
-      <Card />
+      </div>
     </Layout>  
    
   )
